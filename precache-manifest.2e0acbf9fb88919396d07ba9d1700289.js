b19f5321f3d634d47e7250b86b608091b592a059
MatiFalcone <MatiFalcone@users.noreply.github.com>
2021-11-23 04:52:02 +0000

    deploy: 24fd201858e3ad1669dbb1122155a306729a5379

---
+++ precache-manifest.2e0acbf9fb88919396d07ba9d1700289.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5f79558a059f4f7f46a",
+    "url": "/cryptoboys-nft-marketplace/static/css/main.c750cf70.chunk.css"
+  },
+  {
+    "revision": "a5f79558a059f4f7f46a",
+    "url": "/cryptoboys-nft-marketplace/static/js/main.a5f79558.chunk.js"
+  },
+  {
+    "revision": "8a8f0586409faa06c0d3",
+    "url": "/cryptoboys-nft-marketplace/static/js/1.8a8f0586.chunk.js"
+  },
+  {
+    "revision": "8bf4deafcc65d566b658",
+    "url": "/cryptoboys-nft-marketplace/static/js/runtime~main.8bf4deaf.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/cryptoboys-nft-marketplace/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/cryptoboys-nft-marketplace/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "ca04683178d59a022565390e88fafd02",
+    "url": "/cryptoboys-nft-marketplace/index.html"
+  }
+];
