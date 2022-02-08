2a22d56fa0d04e90046c5b1c6381b70cbfd710fd
Matias Falcone <matias.falcone@gmail.com>
2022-02-08 17:05:31 +1100

    milkomeda

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,8 @@
 require("babel-register");
 require("babel-polyfill");
+require("dotenv").config();
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+const privateKey = process.env.PK;
 
 module.exports = {
   networks: {
@@ -12,25 +12,11 @@ module.exports = {
       network_id: "*", // Match any network id
     },
     milkomeda: {
-      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
-      network_id: 97,
+      provider: () => new HDWalletProvider(privateKey, "https://use-util.cloud.milkomeda.com:8555"),
+      network_id: 200101,
       confirmations: 10,
       timeoutBlocks: 200,
       skipDryRun: true
-    },
-    bsc: {
-      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
-      network_id: 56,
-      confirmations: 10,
-      timeoutBlocks: 200,
-      skipDryRun: true
-    },
-    dev: {
-      provider: () => new HDWalletProvider(privateKeys, "https://babel-api.testnet.iotex.io", 0, 1),
-      network_id: 4690,    // IOTEX mainnet chain id 4689, testnet is 4690
-      gas: 8500000,
-      gasPrice: 1000000000000,
-      skipDryRun: true,
     }
   },
   contracts_directory: "./src/contracts/",
